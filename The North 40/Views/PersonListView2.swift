83a9a18c28a32937403432df3b2eb8cb0ef564bd
ahballif <addisonballif@gmail.com>
2024-02-19 15:58:30 -0700

    bug fixes. I'm about to work on sharing

---
+++ The North 40/Views/PersonListView2.swift
@@ -0,0 +1,18 @@
+//
+//  PersonListView2.swift
+//  The North 40
+//
+//  Created by Addison Ballif on 1/23/24.
+//
+
+import SwiftUI
+
+struct PersonListView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonListView2()
+}
